3646b1d513939d0f098f6877bb0a44e2655f36ac
Eren <manjeetsharma9257@gmail.com>
2026-01-29 15:54:37 +0530

    form updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 // ===================
 // Middleware
 // ===================
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
